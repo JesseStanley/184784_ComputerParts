fdf97605dc190fb91863fd168784b55e0178c8e6
JesseStanley <47399968+JesseStanley@users.noreply.github.com>
2019-02-26 11:29:54 -0500

    most of my project

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿/*Jesse Stanley
+ * 2/26/2019
+ * Components for McTavish's computer
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _184784_ComputerParts
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCpu_Click(object sender, RoutedEventArgs e)
+        {
+            txtcompontent.Text = "hello" +
+                "allah" +
+                "ooo";
+
+        }
+
+        private void MotherBoardBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CaseBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MemoryBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PowerSupplyBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OpticalDriveBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GraphicsCard_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GraphicsCardBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void StorageBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
